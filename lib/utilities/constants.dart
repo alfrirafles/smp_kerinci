831f4f0191a40f44b456fe8d44f55a562315b9cb
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-06-27 13:09:31 +0700

    Updated constants

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
 const kTopNavBackgroundColor = Colors.green;
+const kImageAssetsSourceDirectory = 'assets/images/';
+
+const Set<MaterialState> kButtonInteractiveStates = <MaterialState>{
+  MaterialState.pressed,
+  MaterialState.hovered,
+  MaterialState.focused,
+};
+const double kButtonVerticalPadding = 15;
+const double kButtonHorizontalPadding = 25;
+const kButtonBorderStyle = BorderStyle.solid;
+const kButtonBorderColor = Colors.green;
+
+const kLandingImageWidthFactor = 0.6;
+const kLandingImageHeightFactor = 0.25;
+
+enum Orientation {
+  leftToRight,
+  rightToLeft,
+}
