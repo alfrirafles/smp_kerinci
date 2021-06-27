36e53f563ad00639815cc5356a8f89c4689d1dda
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-06-27 11:20:08 +0700

    Implemented calendar image to landing page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smp_kerinci/pages/templates/base_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -9,8 +10,20 @@ class LandingPage extends StatelessWidget {
     return BasePage(
       topNavTitle: 'SMP Kerinci',
       body: Container(
-        child: Center(
-          child: Text('Sample Text'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Container(
+                  child: SvgPicture.asset(
+                    'assets/images/calendar_flatline.svg',
+                    semanticsLabel: 'Calendar Logo',
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
