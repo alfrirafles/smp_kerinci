92d47c3992c9b12c8f2e7117e24892122c874c50
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-06-27 20:58:32 +0700

    Added starting static calendar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smp_kerinci/pages/templates/base_page.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
 
 class CalendarPage extends StatelessWidget {
   @override
@@ -22,7 +24,23 @@ class CalendarPage extends StatelessWidget {
       ],
       body: Container(
         child: Center(
-          child: Text('Sample Text'),
+          child: TableCalendar(
+            locale: 'id_ID',
+            firstDay: DateTime.utc(2021, 1, 1),
+            lastDay: DateTime.utc(2100, 12, 31),
+            focusedDay: DateTime.now(),
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+            calendarStyle: CalendarStyle(
+              holidayTextStyle: TextStyle(color: Colors.red),
+              weekendTextStyle: TextStyle(color: Colors.red),
+            ),
+            daysOfWeekStyle: DaysOfWeekStyle(
+                dowTextFormatter: (date, locale) =>
+                    DateFormat.E(locale).format(date)[0]),
+          ),
         ),
       ),
     );
