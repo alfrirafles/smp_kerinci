8762e9297baac51206a8573e7d9c3b8c805ddac4
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-06-27 13:10:04 +0700

    Completed landing page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:smp_kerinci/pages/templates/base_page.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smp_kerinci/utilities/constants.dart' as constants;
+import 'package:smp_kerinci/components/landing_banner.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return BasePage(
       topNavTitle: 'SMP Kerinci',
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Container(
-                  child: SvgPicture.asset(
-                    'assets/images/calendar_flatline.svg',
-                    semanticsLabel: 'Calendar Logo',
-                  ),
-                ),
-              ],
+            LandingBanner(
+              buttonTitle: 'Kalender',
+              buttonAction: () {},
+              imageAssetName: 'calendar_flatline.svg',
+              imageAssetLabel: 'Calendar Logo',
+              orientation: constants.Orientation.leftToRight,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.20,
+            ),
+            LandingBanner(
+              buttonTitle: 'Pengumuman',
+              buttonAction: () {},
+              imageAssetName: 'news_flatline.svg',
+              imageAssetLabel: 'Announcement Logo',
+              orientation: constants.Orientation.rightToLeft,
             ),
           ],
         ),
