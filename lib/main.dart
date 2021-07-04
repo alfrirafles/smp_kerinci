8e3437b01c11202feabdfc007c616e54b9a2d752
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-07-04 17:44:47 +0700

    Added provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:smp_kerinci/pages/announcement_page.dart';
 import 'utilities/routes.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:provider/provider.dart';
+import 'package:smp_kerinci/utilities/event_handler.dart';
 
 void main() {
   setPathUrlStrategy();
@@ -14,13 +16,16 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '$landingPageRoute': (context) => LandingPage(),
-        '$calendarPageRoute': (context) => CalendarPage(),
-        '$announcementPageRoute': (context) => AnnouncementPage(),
-      },
+    return Provider<EventHandler>(
+      create: (_) => EventHandler(),
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '$landingPageRoute': (context) => LandingPage(),
+          '$calendarPageRoute': (context) => CalendarPage(),
+          '$announcementPageRoute': (context) => AnnouncementPage(),
+        },
+      ),
     );
   }
 }
