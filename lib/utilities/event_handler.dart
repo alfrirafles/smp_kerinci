b94ad53ebc8b51f39e186572c1ac0faf343010d3
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-07-04 17:42:40 +0700

    Added event handler utility

---
+++ lib/utilities/event_handler.dart
@@ -0,0 +1,20 @@
+import 'package:smp_kerinci/models/event.dart';
+import 'package:flutter/material.dart';
+
+class EventHandler {
+  final Map<DateTime, List<Event>> events = {
+    DateTime.now().add(Duration(days: 1)): [
+      Event(
+          title: 'Hello world',
+          date: DateTime.now(),
+          markingColor: Colors.blue),
+    ],
+    DateTime.now(): [
+      Event(
+        title: 'Event 2',
+        date: DateTime.now(),
+        markingColor: Colors.red,
+      )
+    ]
+  };
+}
