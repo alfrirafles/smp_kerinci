5918648222d4c20c0678a9abcd260b711050b0c0
Alfriyadi Rafles <fradcareer@gmail.com>
2021-07-27 18:37:11 +0700

    Refactor: code tidying up and using constants.dart

--- lib/components/create_item_modal.dart
+++ lib/components/create_item_modal.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:smp_kerinci/utilities/constants.dart';
 
 class CreateItemModal extends StatelessWidget {
   final Color? buttonColor;
   final String? buttonDescription;
   final List<Widget>? modalContent;
 
-  CreateItemModal(
-      {this.buttonColor, this.buttonDescription, required this.modalContent});
+  CreateItemModal({
+    this.buttonColor,
+    this.buttonDescription,
+    required this.modalContent,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +18,42 @@ class CreateItemModal extends StatelessWidget {
 
     if (buttonDescription != null) {
       modalButton = FloatingActionButton.extended(
-        backgroundColor: buttonColor == null ? Colors.blue : buttonColor!,
+        backgroundColor:
+            buttonColor == null ? kModalButtonDefaultColor : buttonColor!,
         label: Text(
           buttonDescription!,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
+          style: kModalButtonTextStyle,
         ),
         icon: Icon(Icons.add),
         onPressed: () => showModalBottomSheet<void>(
           context: context,
           builder: (BuildContext context) => Container(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: modalContent!),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: modalContent!,
+            ),
           ),
         ),
       );
     } else {
       modalButton = FloatingActionButton(
         child: Icon(Icons.add),
-        backgroundColor: buttonColor == null ? Colors.blue : buttonColor!,
+        backgroundColor:
+            buttonColor == null ? kModalButtonDefaultColor : buttonColor!,
         onPressed: () => showModalBottomSheet<void>(
           context: context,
           builder: (BuildContext context) => Container(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: modalContent!),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: modalContent!,
+            ),
           ),
         ),
       );
     }
 
     return Padding(
-      padding: EdgeInsets.only(bottom: 20, right: 20),
+      padding: kModalButtonPadding,
       child: Align(
         alignment: Alignment.bottomRight,
         child: modalButton,
